2481f4a88de3bc6dbea73db74495a4c644eb0f29
ZzAZz4 <esteban.villacorta@utec.edu.pe>
2021-08-05 06:01:42 -0500

    kkmkmkm

--- optimal.py
+++ optimal.py
@@ -2,46 +2,9 @@ from __future__ import annotations
 from dataclasses import *
 from typing import *
 from itertools import groupby
-import sys
 from math import inf
 
-@dataclass
-class GenSPT:
-    alpha: Optional[int] = None
-    children: dict[str, GenSPT] = field(default_factory=dict)
-
-# self descriptive (the behaviour, not the implementation)
-def print_tree(root_node: Optional[GenSPT], file=sys.stdout):
-    @dataclass
-    class Context:
-        root_node: GenSPT
-        level: int
-        c: str
-        children: Optional[Iterator[str, Optional[GenSPT]]] = None
-        caller: Optional[Context] = None
-        skip_print: bool = False
-
-    self = Context(root_node, 0, "")
-    while self is not None:
-        if not issubclass(type(self.root_node), GenSPT):
-            print("  " * self.level + self.c + repr(self.root_node), file=file)
-            self = self.caller
-            continue
-        
-        if not self.skip_print: 
-            print("  " * self.level + self.c + f"GenSPT(alpha={self.root_node.alpha}, children=" + "{", file=file)
-            self.skip_print = True
-
-        if self.children is None: 
-            self.children = iter(self.root_node.children.items())
-
-        for char, child in self.children:
-            call = Context(child, self.level + 1, char + ": ", caller=self)
-            self = call
-            break
-        else:
-            print("  " * self.level + "}", file=file)
-            self = self.caller
+from trie import *
 
 # returns true if all elements in iterable are the same
 # complexity O(len(iterable))
@@ -115,7 +78,7 @@ def OPT_BUILD(strings, i, j):
             root, edges = GenSPT(r), 0
 
             for i_, j_ in C(i, j, r):                                          #O(|sigma|)
-                branch, branch_edges = OPT_BUILD_HAT(i_, j_)                   #T(n)
+                branch, branch_edges = OPT_BUILD_HAT(i_, j_)                   
                 Ki_j_ = K(i_, j_)
                 edges += branch_edges + len(Ki_j_) - len(Kij)
 
